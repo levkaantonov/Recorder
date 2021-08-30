2f253fcbc0ca7715329796c3d26c7c5e3ec4d74a
levkaantonov <levkaantonov@gmail.com>
2021-08-30 16:11:53 +0300

    initial commit added empty project

---
+++ app/src/main/java/levkaantonov/com/study/recorder/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package levkaantonov.com.study.recorder.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import levkaantonov.com.study.recorder.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
